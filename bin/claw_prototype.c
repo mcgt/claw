401c2c18a25837431e22d1681ac4d1720db339dc
emcgant <anthony.mcglone@ericsson.com>
2016-04-10 03:08:54 +0100

    Modidfying claw protoype for parameters

--- bin/claw_prototype.c
+++ bin/claw_prototype.c
@@ -1,4 +1,3 @@
-/* We want POSIX.1-2008 + XSI, i.e. SuSv4, features */
 #define _XOPEN_SOURCE 700
 
 #include <stdlib.h>
@@ -10,18 +9,6 @@
 #include <errno.h>
 
 
-/* POSIX.1 says each process has at least 20 file descriptors.
- *  * Three of those belong to the standard streams.
- *   * Here, we use a conservative estimate of 15 available;
- *    * assuming we use at most two for other uses in this program,
- *     * we should never run into any problems.
- *      * Most trees are shallower than that, so it is efficient.
- *       * Deeper trees are traversed fine, just a bit slower.
- *        * (Linux allows typically hundreds to thousands of open files,
- *         *  so you'll probably never see any issues even if you used
- *          *  a much higher value, say a couple of hundred, but
- *           *  15 is a safe, reasonable value.)
- *           */
 #ifndef USE_FDS
 #define USE_FDS 15
 #endif
@@ -102,15 +89,21 @@ int print_entry(const char *filepath, const struct stat *info,
 }
 
 
-int print_directory_tree(const char *const dirpath)
+int start(const char *const dirpath, const char *const extension)
 {
     int result;
 
-    /* Invalid directory path? */
+    /* If it's a bad path */
     if (dirpath == NULL || *dirpath == '\0')
         return errno = EINVAL;
 
-    result = nftw(dirpath, print_entry, USE_FDS, FTW_PHYS);
+    /* A file extension should be less than 10
+     * characters. Should be lower case and only
+     * contain text
+    */
+
+
+    result = nftw(dirpath, claw, USE_FDS, FTW_PHYS);
     if (result >= 0)
         errno = result;
 
@@ -121,22 +114,16 @@ int main(int argc, char *argv[])
 {
     int arg;
 
-    if (argc < 2) {
+    if (argc != 3) {
+        fprintf("claw takes two arguments... <directory> <file_extension>\n");
+        fprintf("e.g. claw /root/ log");
+        fprintf("     This will get all .log files under the root directory. Careful!")
+        return EXIT_FAILURE;
+    } 
 
-        if (print_directory_tree(".")) {
-            fprintf(stderr, "%s.\n", strerror(errno));
-            return EXIT_FAILURE;
-        }
-
-    } else {
-
-        for (arg = 1; arg < argc; arg++) {
-            if (print_directory_tree(argv[arg])) {
-                fprintf(stderr, "%s.\n", strerror(errno));
-                return EXIT_FAILURE;
-            }
-        }
-
+    if (start(argv[1], argv[2])) {
+        fprintf(stderr, "%s.\n", strerror(errno));
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
