3ab6992da84d3bbfbf862cdaa4be41fb5bc272b2
emcgant <anthony.mcglone@ericsson.com>
2016-04-17 18:15:15 +0100

    adding claw function

--- src/claw.c
+++ src/claw.c
@@ -4,9 +4,17 @@ int main(int argc, char *argv[]) {
 
 	int i;
     for (i = 0; i < argc; ++i) {
-        printf(argv[i]);
-        printf("\n");
+        
     }
+    claw(argv[1], argv[2]);
+
 
     return 0;
+}
+
+
+int claw(char start_dir[], char extension[]){
+
+	printf(start_dir);
+	printf(extension);
 }
