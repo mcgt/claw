2872e8270f71eae7c5cfa79279dda14d38b5fc4b
emcgant <anthony.mcglone@ericsson.com>
2016-04-18 03:26:23 +0100

    UPdate to claw

--- src/claw.c
+++ src/claw.c
@@ -2,10 +2,28 @@
 
 int main(int argc, char *argv[]) {
 
-	int i;
-    for (i = 0; i < argc; ++i) {
-        
+
+    // if helptext in argv[1] print 1
+    if (argc == 2) {
+        if (strcmp(argv[1], "--h") == 0 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h")) {
+            printf("\n./claw <search_directory> <file_extension>\n");
+
+        }
     }
+
+    // if length of argv less than three
+    // print out help message
+
+
+    // if length of argv is greater than 3
+    // too many arguments
+
+    // validate
+    // if validate returns false
+    // exit
+
+
+
     claw(argv[1], argv[2]);
 
 
@@ -15,6 +33,6 @@ int main(int argc, char *argv[]) {
 
 int claw(char start_dir[], char extension[]){
 
-	printf(start_dir);
-	printf(extension);
+	//printf(start_dir);
+	//printf(extension);
 }
